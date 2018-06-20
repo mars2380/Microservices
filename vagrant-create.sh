897b0d130bbd2252f20f30842ca5b31d25ac9385
Alessandro Di Micoli <adimicoli@hotmail.com>
2018-06-21 00:07:24 +0100

    Set Vagrant installation

--- vagrant-create.sh
+++ vagrant-create.sh
@@ -1,3 +1,36 @@
+#!/bin/bash
+function CHECK_VAGRANT {
+	VERSION=$(vagrant --version)
+}
+function CHECK_VIRTUALBOX {
+	WHEREIS=$(whereis virtualbox | awk -F":" '{print $NF}')
+}
+function install_linux {
+	CHECK_VAGRANT
+	if [ ! -z "$VERSION" ]; then
+		echo "Vagrant is installed"
+		sleep 3
+	else
+		echo "NO"
+		sudo apt update && sudo apt install vagrant -y
+	fi
+	CHECK_VIRTUALBOX
+	if [ ! -z  "$WHEREIS" ]; then
+		echo "VirtualBox is installed"
+		sleep 3
+	else
+		echo "NO"
+		sudo apt update && sudo apt install -y virtualbox virtualbox-qt virtualbox-guest-utils
+	fi	
+}
+
+oscheck="$(uname)"
+if [[ "$oscheck" == "Linux" ]]; then
+	install_linux
+else
+	echo "No Linux or Apple"
+fi
+
 vagrant up
 vagrant box update
 vagrant provision
