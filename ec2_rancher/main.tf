c546875bb2018f9c6803944ffd12e5efc1058e5c
Alessandro Di Micoli <adimicoli@gmail.com>
2018-06-25 14:52:47 +0100

    Test Travis

--- ec2_rancher/main.tf
+++ ec2_rancher/main.tf
@@ -8,32 +8,6 @@ resource "aws_key_pair" "auth" {
     public_key = "${file("~/.ssh/id_rsa.pub")}"
   }
 
-resource "aws_instance" "EC2" {
-	ami = "ami-f90a4880"
-	instance_type = "t2.micro"
-	vpc_security_group_ids = ["${aws_security_group.default.id}"]
-  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
-
-	connection {
-    user = "ubuntu"
-    }
-
-	key_name = "Terraform_Key"
-
-  root_block_device {
-      volume_type = "gp2"
-      volume_size = 30
-  }
-
-tags {
-		Name = "Rancher"
-	}
-
-  provisioner "local-exec" {
-	  command = "ansible-playbook -i hosts ec2_rancher.yml -e 'ansible_python_interpreter=/usr/bin/python3' --tags=kubernetes"
-	  }
-}
-
 resource "aws_eip" "EIP" {
   vpc = true
 }
@@ -41,3 +15,30 @@ resource "aws_eip" "EIP" {
 output "ip" {
   value = "${aws_eip.EIP.public_ip}"
 }
+
+resource "aws_instance" "EC2" {
+  ami = "ami-f90a4880"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = ["${aws_security_group.default.id}"]
+  vpc_security_group_ids = ["${aws_security_group.kubernetes.id}"]
+
+  connection {
+    user = "ubuntu"
+    }
+
+  key_name = "Terraform_Key"
+
+  root_block_device {
+      volume_type = "gp2"
+      volume_size = 30
+  }
+
+tags {
+    Name = "Rancher"
+  }
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i hosts ec2_rancher.yml -e 'ansible_python_interpreter=/usr/bin/python3' --tags=kubernetes"
+    }
+}
+
